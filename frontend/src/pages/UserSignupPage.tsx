9849059bf70cd01d90d39888557b0917d24a5b25
Sangho Min <shmin@builton.co.kr>
2021-07-12 20:36:22 +0900

    # frontend: trivial bootstrap

--- frontend/src/pages/UserSignupPage.tsx
+++ frontend/src/pages/UserSignupPage.tsx
@@ -61,34 +61,49 @@ class UserSignUpPage extends React.Component<State> {
 
     render() {
         return (
-            <div>
-                <h1>Sign up</h1>
-                <div>
-                    <input placeholder="Your display name"
+            <div className="container">
+                <h1 className="text-center">Sign up</h1>
+                <div className="col-12 mb-3">
+                    <label>Display Name</label>
+                    <input
+                        className="form-control"
+                        placeholder="Your display name"
                         value={this.state.displayName}
                         onChange={this.onChangeDisplayName}
                     />
                 </div>
-                <div>
-                    <input placeholder="Your username"
+                <div className="col-12 mb-3">
+                    <label>User Name</label>
+                    <input
+                        className="form-control"
+                        placeholder="Your username"
                         value={this.state.username}
                         onChange={this.onChangeUsername}
                     />
                 </div>
-                <div>
-                    <input placeholder="Your password" type="password"
+                <div className="col-12 mb-3">
+                    <label>Password</label>
+                    <input
+                        className="form-control"
+                        placeholder="Your password" type="password"
                         value={this.state.password}
                         onChange={this.onChangePassword}
                     />
                 </div>
-                <div>
-                    <input placeholder="Repeat your password" type="password"
+                <div className="col-12 mb-3">
+                    <label>Password Repeat</label>
+                    <input
+                        className="form-control"
+                        placeholder="Repeat your password" type="password"
                         value={this.state.passwordRepeat}
                         onChange={this.onChangePasswordRepeat}
                     />
                 </div>
-                <div>
-                    <button onClick={this.onClickSignup}>Sign up</button>
+                <div className="text-center">
+                    <button
+                        className="btn btn-primary"
+                        onClick={this.onClickSignup}>Sign up
+                    </button>
                 </div>
             </div>
         )
